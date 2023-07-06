2d09f4b78d72089ce10313457fa0dc7f8b5a454e
Adam <ajm27@pdx.edu>
2023-07-06 12:39:14 -0700

    implemented adc into panel

--- panel/solar-simulator-panel.py
+++ panel/solar-simulator-panel.py
@@ -1,5 +1,6 @@
 from Adafruit_I2C import Adafruit_I2C
 import adafruit_mcp4728
+import Adafruit_ADS1x15
 import socketio
 import argparse
 import json
@@ -8,6 +9,7 @@ import busio
 import time
 import Adafruit_BBIO.PWM as PWM
 from numpy import linspace, uint16
+GAIN = 1
 global system_state
 system_state = 0
 
@@ -52,6 +54,7 @@ args = parser.parse_args()
 # Setup I2C
 i2c = busio.I2C('I2C1_SCL', 'I2C1_SDA')
 mcp4728 = adafruit_mcp4728.MCP4728(i2c)
+adc = Adafruit_ADS1x15.ADS1015()
 
 # Setup PWM
 BB_FREQ = 250e3
@@ -99,8 +102,15 @@ def set_panel(level):
         mcp4728.channel_c.value = steps[2][level]
         mcp4728.channel_d.value = steps[3][level]
         PWM.set_duty_cycle(PWM_PIN, level)
-    
-    sio.emit('panel_response', [args.clientid, 'temp', 'photo'])
+        values = [0] * 4
+        for i in range(4):
+        # Read the specified ADC channel using the previously set gain value.
+            values[i] = adc.read_adc(i, gain=GAIN)
+
+        print(values)
+        temp_values = (values[0], values[1], values[2])
+        photo_value = values[3]
+    sio.emit('panel_response', [args.clientid, temp_values, photo_value])
     os.system(f'echo 1 > {led2}/brightness')
     os.system(f'echo 0 > {led2}/brightness')
 
