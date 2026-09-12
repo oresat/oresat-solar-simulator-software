2cf2bb5b026ab6a0f9df0d61ee1b8b2ed262743e
Bryan Borgeson <bryborge@proton.me>
2026-09-12 15:19:12 -0700

    Remove boot.py, which restated CircuitPython's USB defaults
    boot.py called usb_cdc.enable(console=True, data=False), which is the state
    CircuitPython already boots into: the console channel is on and the data
    channel is off. The file changed nothing about how the board starts, but
    it still had to be built, copied to the board and read as if it mattered.
    
    Delete it and drop it from COPY_SRCS.

--- src/solar_simulator/boot.py
+++
@@ -1,8 +0,0 @@
-"""Bootstrap the microcontroller on start up (hard reset and power on).
-
-Docs: https://learn.adafruit.com/circuitpython-essentials/circuitpython-storage
-"""
-
-import usb_cdc
-
-usb_cdc.enable(console=True, data=False)
